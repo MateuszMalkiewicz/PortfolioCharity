f836b4f1b9817d4857b8eb65f544bb5708c30689
Mateusz Małkiewicz <mathew.malkiewicz@gmail.com>
2019-09-17 15:42:31 +0200

    modified templates and made first views

--- Charity/urls.py
+++ Charity/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import charity_donation.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.LandingPageView.as_view()),
+    path('add_donation/', views.AddDonationView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('register/', views.RegisterView.as_view()),
 ]
